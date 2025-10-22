62717c96acee803acb34883cc2c61117cb89a7f2
Clemente Velasco <clemente.velasco@gmail.com>
2025-10-22 14:09:20 -0400

    ENH: singularity support
    Singularity support added for SVRTK reconstructions of b0 and b1 data.

--- svrtk-dexec.sh
+++ svrtk-dexec.sh
@@ -2,7 +2,7 @@
 
 show_help () {
 cat << EOF
-    USAGE: sh ${0##*/} [-nob0 || -nob1 ]] -- [DWI Case Dir]
+    USAGE: sh ${0##*/} [-nob0 || -nob1 || -s ]] -- [DWI Case Dir]
     Incorrect input supplied
     This script is used after running svrtk-bgen.sh to generate 'run-svrtk.sh's for a fetal diffusion pipeline case
     Supply a dwi directory (the one named with the subject id and has folders like 'volumes', 'svrtk', and 'b0b1')
@@ -10,6 +10,7 @@ cat << EOF
     Creates a detached SVRTK docker image, and then uses it to execute the run script, then deletes the container
     -nob0       Skips b0 recon
     -nob1       Skips b1 recon
+    -s		Run container with singularity instead of docker (necessary for cluster)
 EOF
 }
 
@@ -29,6 +30,9 @@ while :; do
         -nob1|--nob1)
             let nob1=1
             ;;
+	-s|--sing)
+	    let SING=1
+	    ;;
         --) # end of optionals
             shift
             break
@@ -63,35 +67,52 @@ if [[ ! -n $runs ]] ; then
     die "error: run-svrtk.sh not found in the subj/svrtk/b0 or b1 directories"
 fi
 
-# Path to mount inside container
-conpath="/home/data"
-# random string
-dockname="SVRTK-$RANDOM"
+# initialize Docker
+if [[ $SING -ne 1 ]] ; then
+	# Path to mount inside container
+	conpath="/home/data"
+	# random string
+	dockname="SVRTK-$RANDOM"
+	echo "app=Docker"
+	echo "Container will be named $dockname"
+	echo "Mount path within container: $conpath"
+	echo "Initializing SVRTK Docker container"
+	docker run -id --name $dockname --rm --mount type=bind,source=${mpath},target=${conpath} fetalsvrtk/svrtk /bin/bash
+	echo
+else echo "app=Singularity"
+fi
 
-echo "Container will be named $dockname"
-echo "Mount path within container: $conpath"
-echo "Initializing SVRTK Docker container"
-docker run -id --name $dockname --rm --mount type=bind,source=${mpath},target=${conpath} fetalsvrtk/svrtk /bin/bash
-echo
 if [[ $nob0 -ne 1 ]] ; then
-    echo "Executing b0 SVRTK recon within container"
-    date
-    docker exec -t -i -w /home/data $dockname sh -c "sh svrtk/b0/run-svrtk.sh"
-    echo
-    echo "B0 recon done"
+	echo "Executing b0 SVRTK recon within container"
+	date
+	if [[ $SING -eq 1 ]] ; then
+		singularity exec --cwd $mpath docker://fetalsvrtk/svrtk /bin/sh -c "sh ${mpath}/svrtk/b0/run-svrtk.sh ; rmdir tmp-file-exchange ; mv -v image?.nii.gz init.nii.gz log-registration.txt masked.nii.gz output-metric*txt svrtk/b0/"
+		else
+		docker exec -t -i -w /home/data $dockname sh -c "sh svrtk/b0/run-svrtk.sh ; rmdir tmp-file-exchange ; mv -v image0.nii.gz image1.nii.gz image2.nii.gz init.nii.gz log-registration.txt masked.nii.gz output-metric*txt svrtk/b0/"
+	fi
+	echo
+	echo "B0 recon done"
 else echo "--nob0 is set, skipping b0 reconstruction"
 fi
+
 if [[ $nob1 -ne 1 ]] ; then
-    echo "Executing b1 SVRTK recon within container"
-    date
-    docker exec -t -i -w /home/data $dockname sh -c "sh svrtk/b1/run-svrtk.sh"
-    echo
-    echo "B1 recon done"
+	echo "Executing b1 SVRTK recon within container"
+	date
+	if [[ $SING -eq 1 ]] ; then
+		singularity exec --cwd $mpath docker://fetalsvrtk/svrtk /bin/sh -c "sh ${mpath}/svrtk/b1/run-svrtk.sh ; rmdir tmp-file-exchange ; mv -v image?.nii.gz init.nii.gz log-registration.txt masked.nii.gz output-metric*txt svrtk/b1/"
+	else
+		docker exec -t -i -w /home/data $dockname sh -c "sh svrtk/b1/run-svrtk.sh ; rmdir tmp-file-exchange ; mv -v image0.nii.gz image1.nii.gz image2.nii.gz init.nii.gz log-registration.txt masked.nii.gz output-metric*txt svrtk/b1/"
+	fi
+	echo
+	echo "B1 recon done"
 else echo "--nob1 is set, skipping b1 reconstruction":
 fi
-echo "Stopping docker image"
-docker stop $dockname
-echo
+
+if [[ $SING -ne 1 ]] ; then
+	echo "Stopping docker image"
+	docker stop $dockname
+	echo
+fi
 
 if [[ ! -f ${mpath}/svrtk/b0/SVRTK-dwi_b0_${id}.nii.gz || ! -f ${mpath}/svrtk/b1/SVRTK-dwi_b1_${id}.nii.gz || ! -f ${mpath}/svrtk/b0/image0.nii.gz ]] ; then
 	echo "WARNING: At least one of b0_1, b0_2 or b1 were not generated. Check output"
@@ -99,6 +120,6 @@ fi
 
 # Copy SVRTK recon outputs to b0b1 folder
 cp ${mpath}/svrtk/b0/SVRTK-dwi_b0_${id}.nii.gz -vup ${mpath}/b0b1/dwi_b0_${id}.nii.gz
-cp ${mpath}/svrtk/b0/image0.nii.gz -vup ${mpath}/b0b1_b0_${id}_tensor.nii.gz
+cp ${mpath}/svrtk/b0/image0.nii.gz -vup ${mpath}/b0b1/b0_${id}_tensor.nii.gz
 cp ${mpath}/svrtk/b1/SVRTK-dwi_b1_${id}.nii.gz -vup ${mpath}/b0b1/dwi_b1_${id}.nii.gz
-gzip -d ${mpath}/b0b1_b0_${id}_tensor.nii.gz # computeTensor binary needs this uncompressed
+gzip -d -f ${mpath}/b0b1/b0_${id}_tensor.nii.gz # computeTensor binary needs this uncompressed
--- svrtk-dgen.sh
+++ svrtk-dgen.sh
@@ -2,31 +2,69 @@
 
 show_help () {
 cat << EOF
-    USAGE: sh ${0##*/} [subj converted data dir] [mask for recon]
+    USAGE: sh ${0##*/} [-n image limit per scan] -- [subj converted data dir] [mask for recon]
     Incorrect argument supplied
+
+    -n		Sets image limit for each volume folder. For example, "-n 6" for a subject with
+		three diffusion scans would mean only 18 total volumes are reconstructed.
 EOF
 }
+
+die() {
+    printf '%s\n' "$1" >&2
+    exit 1
+}
+while :; do
+    case $1 in
+        -h|-\?|--help)
+            show_help # help message
+            exit
+            ;;
+        -n)
+            if [[ "$2" -gt 0 ]] ; then
+                let LIMIT=$2 # Specify how many volumes to take from each scan
+                shift
+            else
+                die 'error: -n argument should be a number of volumes to include from each series'
+            fi
+            ;;
+        --) # end of optionals
+            shift
+            break
+            ;;
+        -)?*
+            printf 'warning: unknown option (ignored: %s\m' "$1" >&2
+            ;;
+        *) # default case, no optionals
+            break
+    esac
+    shift
+done
+
+
 
 if [ $# -ne 2 ]; then
     show_help
     exit
 fi 
 
+if [[ ! -n $LIMIT ]] ; then let $LIMIT=999 ; fi # default limit (aka no limit)
+
 data=`readlink -f $1`
 subj=`dirname $data`
 id=`basename $subj`
 mask="$2"
 svrtk="svrtk"
-volumes="${subj}/svrtk/volumes"
+volumes="${subj}/volumes"
 mkdir -pv ${subj}/svrtk ${volumes}
 cp $mask -v ${subj}/${svrtk}/mask_svrtk.nii.gz
-svrmask="/home/data/${svrtk}/mask_svrtk.nii.gz"
+svrmask="${svrtk}/mask_svrtk.nii.gz"
 b0dir=${svrtk}/b0
 b1dir=${svrtk}/b1
 runb0=${subj}/${b0dir}/run-svrtk.sh
 runb1=${subj}/${b1dir}/run-svrtk.sh
-b0SVR="/home/data/${b0dir}/SVRTK-dwi_b0_${id}.nii.gz"
-b1SVR="/home/data/${b1dir}/SVRTK-dwi_b1_${id}.nii.gz"
+b0SVR="${b0dir}/SVRTK-dwi_b0_${id}.nii.gz"
+b1SVR="${b1dir}/SVRTK-dwi_b1_${id}.nii.gz"
 b0list="${subj}/${b0dir}/b0list.txt"
 b1list="${subj}/${b1dir}/b1list.txt"
 mkdir -pv ${subj}/${b0dir} ${subj}/${b1dir}
@@ -48,14 +86,18 @@ for dwi in ${data}/* ; do
         let x=0 # this assumes the volumes are named/numbered vol_0000, vol_0001, etc
         # Read the bvals text file for bvalues
         for b in `cat ${dwi}/bvals` ; do 
-            lead=$(printf "%04d" $x) # changes the index to have four leading 0's
-            echo ${split}/vol_${lead}.nii.gz $b # this is the volume-bvalue combo
-            # if 0, use for B0 recon, if greater than 0, use for B1 recon
-            if [[ $b -eq 0 ]] ; then
-                echo ${split}/vol_${lead}.nii.gz >> ${b0list}
-            elif [[ $b > 0 ]] ; then
-                echo ${split}/vol_${lead}.nii.gz >> ${b1list}
-            fi
+		if [[ $x -lt $LIMIT ]] ; then
+		    lead=$(printf "%04d" $x) # changes the index to have four leading 0's
+		    echo ${split}/vol_${lead}.nii.gz $b # this is the volume-bvalue combo
+		    # if 0, use for B0 recon, if greater than 0, use for B1 recon
+		    if [[ $b -eq 0 ]] ; then
+			echo ${split}/vol_${lead}.nii.gz >> ${b0list}
+		    elif [[ $b > 0 ]] ; then
+			echo ${split}/vol_${lead}.nii.gz >> ${b1list}
+		    fi
+	    	else
+		    echo "Input vol limit has been reached $x >= $LIMIT" 	
+		fi
             ((x++)) # increase index by one
         done
     fi
@@ -66,30 +108,26 @@ nb0=`wc -l ${b0list} | cut -d' ' -f1`
 nb1=`wc -l ${b1list} | cut -d' ' -f1`
 
 # Write script for b0 recon
-echo "cd /home/data/${b0dir}" >> $runb0
 echo "mirtk reconstruct $b0SVR $nb0 \\" >> $runb0
 for im in `cat $b0list` ; do
-	impath=`echo $im | sed 's,.*svrtk/volumes,svrtk/volumes,g'`
-    echo "/home/data/$impath \\" >> $runb0
+	impath=`echo $im | sed 's,.*volumes,volumes,g'`
+    echo "$impath \\" >> $runb0
 done
 echo "-mask $svrmask \\" >> $runb0
 echo "-svr_only \\" >> $runb0
 echo "-resolution 0.75 \\" >> $runb0
 echo "-iterations 3" >> $runb0
-echo "cd /home/data" >> $runb0
 
 # Write script for b1 recon
-echo "cd /home/data/${b1dir}" >> $runb1 
 echo "mirtk reconstruct $b1SVR $nb1 \\" >> $runb1
 for im in `cat $b1list` ; do
-	impath=`echo $im | sed 's,.*svrtk/volumes,svrtk/volumes,g'`
-    echo "/home/data/$impath \\" >> $runb1
+	impath=`echo $im | sed 's,.*volumes,volumes,g'`
+    echo "$impath \\" >> $runb1
 done
 echo "-mask $svrmask \\" >> $runb1
 echo "-svr_only \\" >> $runb1
 echo "-resolution 0.75 \\" >> $runb1
 echo "-iterations 3" >> $runb1
-echo "cd /home/data" >> $runb1
 
 echo Wrote run scripts:
 echo $runb0
